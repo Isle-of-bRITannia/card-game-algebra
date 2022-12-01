f6b3e02f4dd77d8fa3e1985397459b5864666106
Stephen Adegun (RIT Student) <saa9310@rit.edu>
2022-12-01 17:26:33 -0500

    API WIP; Ran into issue with types and functions

--- index.ts
+++ index.ts
@@ -1,4 +1,4 @@
-import { Card, Deck, Alg } from "./algebra";
+import { Card, Deck, Alg, MultiDeck } from "./algebra";
 
 type DeckToDeck = (deck: Deck) => Deck;
 type DecksToDeck = (...decks: Deck[]) => Deck;
@@ -9,4 +9,32 @@ type CardAPI = {
     draw: DecksToDecks,
     combine: DecksToDeck,
     discard: DecksToDecks
+}
+
+const API: CardAPI = {
+    shuffle: (inputDeck) => {
+        if (inputDeck._tag === "MultiDeck")
+        {
+            for (let i = inputDeck.deck.length - 1; i > 0; i--)
+            {
+                const j = Math.floor(Math.random() * (i + 1));
+                const temp = inputDeck.deck[i];
+                inputDeck.deck[i] = inputDeck.deck[j];
+                inputDeck.deck[j] = temp;
+
+                return inputDeck as Deck;
+            }
+        }
+        else
+        {
+            return inputDeck as Deck;
+        }
+    },
+    draw: () => {},
+    combine: () => {},
+    discard: () => {}
+};
+
+export {
+    API as Card
 }
